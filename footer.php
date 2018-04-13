b387be73fd5eba0de8e4c72b6f16a576b97b3ab2
harley-travis <harley.travis2@gmail.com>
2018-04-12 20:26:36 -0600

    more init files and updates

--- footer.php
+++ footer.php
@@ -3,6 +3,8 @@
         </footer>
 
         <!-- scripts -->
+        <script src="<?php bloginfo('template_directory'); ?>/assets/js/bootstrap.min.js"></script>
+        <script src="<?php bloginfo('template_directory'); ?>/assets/js/particles.min.js"></script>
 
         <?php wp_footer(); ?>
 	</body>
